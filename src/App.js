a21119cfc049a444a1243fdb76ca6a72e3db0100
oomusou <oomusou@gmail.com>
2019-04-02 23:08:04 +0800

    Function component with prevCount

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 const App = () => {
   const [count, setCount] = useState(0);
-  const addCount = () => setCount(count + 1);
+  const addCount = prevState => setCount(prevCount => prevCount + 1);
 
   return (
     <div>
